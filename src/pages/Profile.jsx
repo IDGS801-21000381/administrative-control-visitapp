0cb55c491bdf05465e145c39e469fe6ea2d18f9a
Carlos Martinez <carlos.martinez@visitapp.la>
2025-03-14 17:22:36 -0600

    "creacion de paerfil apenas puse el titulo"

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import MainLayout from "../layouts/MainLayout";
 
 const Profile = () => {
   return (
     <div>
-      Perfil del usuario
+      <MainLayout>
+       <div></div> 
+        <h1>Perfil del usuario</h1>
+
+         </MainLayout>
+
     </div>
   )
 }
